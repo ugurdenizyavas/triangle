116bf5ff9a56f0dff2153a45ee80a10a6ee0d4c0
tryavasu <ugurdeniz.yavas@eu.sony.com>
2014-03-14 15:20:41 +0200

    Configuration

--- essence/Essence-Configuration/src/main/java/com/deniz/framework/configuration/LoggingPropertyPlaceholderConfigurer.java
+++ essence/Essence-Configuration/src/main/java/com/deniz/framework/configuration/LoggingPropertyPlaceholderConfigurer.java
@@ -1,62 +1,76 @@
 package com.deniz.framework.configuration;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 /**
  * <p/>
  * Extends the PropertyPlaceholderConfigurer and collects the final propertys for later logging through ApplicationConfigurationLogger.
- * 
+ * <p/>
  * </p>
+ *
  * @author Deniz Yavas
  */
-public class LoggingPropertyPlaceholderConfigurer extends PropertyPlaceholderConfigurer
-{
+public class LoggingPropertyPlaceholderConfigurer extends PropertyPlaceholderConfigurer {
+
+    private Map<String, String> configurationProperties = new HashMap<String, String>();
+    private List<String> propertyFilenames = new ArrayList<String>();
 
 
-	private Map<String, String> configurationProperties = new HashMap<String, String>();
-	private List<String> propertyFilenames = new ArrayList<String>();
+    @Override
+    protected String resolvePlaceholder(String placeholder, Properties props) {
+        String finalValue = super.resolvePlaceholder(placeholder, props);
+
+        configurationProperties.put(placeholder, finalValue);
+        return finalValue;
+    }
 
 
-	@Override
-	protected String resolvePlaceholder( String placeholder, Properties props )
-	{
-		String finalValue = super.resolvePlaceholder( placeholder, props );
+    public void setConfigurationProperties(Map<String, String> configurationProperties) {
+        this.configurationProperties = configurationProperties;
+    }
 
-		configurationProperties.put( placeholder, finalValue );
-		return finalValue;
-	}
+    public Map<String, String> getConfigurationProperties() {
+        return configurationProperties;
+    }
 
+    @Override
+    public void setLocations(Resource[] locations) {
+        String stage = System.getProperty("app.configuration.stage");
+        if (StringUtils.isEmpty(stage)) {
+            Resource resource = new ClassPathResource("configuration/stages/");
+            try {
+                File file = resource.getFile();
+                File[] listOfFiles = file.listFiles();
+                String propertyFile = listOfFiles[0].getName();
+                logger.info("app.configuration.stage runtime arguments is null, " +
+                        "so app is using " + propertyFile + " for configuration");
+                propertyFilenames.add(propertyFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Resource stageConfiguration = new ClassPathResource("configuration/stages/app." + stage + ".properties");
+            propertyFilenames.add(stageConfiguration.getFilename());
+        }
+        super.setLocations(locations);
+        for (Resource location : locations) {
+            propertyFilenames.add(location.getFilename());
+        }
+    }
 
-	public void setConfigurationProperties( Map<String, String> configurationProperties )
-	{
-		this.configurationProperties = configurationProperties;
-	}
-
-	public Map<String, String> getConfigurationProperties()
-	{
-		return configurationProperties;
-	}
-
-	@Override
-	public void setLocations( Resource[] locations )
-	{
-		super.setLocations( locations );
-		for ( Resource location : locations )
-		{
-			propertyFilenames.add( location.getFilename() );
-		}
-	}
-
-	public List<String> getPropertyFilenames()
-	{
-		return propertyFilenames;
-	}
+    public List<String> getPropertyFilenames() {
+        return propertyFilenames;
+    }
 }
 
--- essence/Essence-Servlet/src/main/java/com/deniz/framework/controller/LoggingTaskControllerTemplate.java
+++ essence/Essence-Servlet/src/main/java/com/deniz/framework/controller/LoggingTaskControllerTemplate.java
@@ -13,97 +13,82 @@ import com.deniz.framework.logging.LoggingHelper;
 /**
  * Base class that is used for all controller classes.
  * <p/>
+ *
  * @author Deniz Yavas
  */
 public abstract class LoggingTaskControllerTemplate extends MultiActionController {
     protected final Logger logger = LoggerFactory.getLogger(getClass());
 
-	protected static final String LOG_EVENT_USER_PRESSED_PREVIOUS = "User pressed previous.";
-	protected static final String LOG_EVENT_USER_PRESSED_NEXT = "User pressed next.";
-	protected static final String LOG_EVENT_USER_PRESSED_SAVE = "User pressed save.";
+    protected static final String LOG_EVENT_USER_PRESSED_PREVIOUS = "User pressed previous.";
+    protected static final String LOG_EVENT_USER_PRESSED_NEXT = "User pressed next.";
+    protected static final String LOG_EVENT_USER_PRESSED_SAVE = "User pressed save.";
 
-	/**
-	 * Logs a service call that deals with a dto object on INFO level.
-	 * 
-	 * @param request
-	 *            The HttpServletRequest that will be used to extract the
-	 *            service url, e.g. "flight.newReportBackOffice/header/next"
-	 * @param abstractDto
-	 *            the dto that was persisted.
-	 * @param event
-	 *            The event that happened, e.g. "User pressed next."
-	 * @param systemReaction
-	 *            How did the system react, e.g.
-	 *            "Show previous task with errors because report contained validation errors."
-	 */
-	protected void logServiceCall(HttpServletRequest request,
-			AbstractDto abstractDto, String event, String systemReaction) {
-		if (abstractDto != null) {
-			String typeAndPk = LoggingHelper.getTypeAndPk(abstractDto);
-			String info = abstractDto.getInfo();
-			logServiceCallInfo(request.getPathInfo(), typeAndPk, info, event,
-					systemReaction);
-		} else {
-			logServiceCallInfo(request.getPathInfo(), null, null, event,
-					systemReaction);
-		}
-	}
+    /**
+     * Logs a service call that deals with a dto object on INFO level.
+     *
+     * @param request        The HttpServletRequest that will be used to extract the
+     *                       service url, e.g. "flight.newReportBackOffice/header/next"
+     * @param abstractDto    the dto that was persisted.
+     * @param event          The event that happened, e.g. "User pressed next."
+     * @param systemReaction How did the system react, e.g.
+     *                       "Show previous task with errors because report contained validation errors."
+     */
+    protected void logServiceCall(HttpServletRequest request,
+                                  AbstractDto abstractDto, String event, String systemReaction) {
+        if (abstractDto != null) {
+            String typeAndPk = LoggingHelper.getTypeAndPk(abstractDto);
+            String info = abstractDto.getInfo();
+            logServiceCallInfo(request.getPathInfo(), typeAndPk, info, event,
+                    systemReaction);
+        } else {
+            logServiceCallInfo(request.getPathInfo(), null, null, event,
+                    systemReaction);
+        }
+    }
 
-	/**
-	 * Logs a service call that deals with a generic object on INFO level.
-	 * 
-	 * This method should only be used in exceptional cases where you do not
-	 * have a dto object. Usually you would use
-	 * ControllerTemplate#logServiceCall.
-	 * 
-	 * @param request
-	 *            The HttpServletRequest that will be used to extract the
-	 *            service url, e.g. "flight.newReportBackOffice/header/next"
-	 * @param businessObjectType
-	 *            e.g. "SearchField"
-	 * @param businessObjectTechnicalPk
-	 *            The technical key used ito identify the business object e.g.
-	 *            "45"
-	 * @param info
-	 *            Headline type info about the business object, e.g.
-	 *            "reportNumber"
-	 * @param event
-	 *            The event that happened, e.g. "User pressed next."
-	 * @param systemReaction
-	 *            How did the system react, e.g.
-	 *            "Show previous task with errors because report contained validation errors."
-	 * @see LoggingTaskControllerTemplate#logServiceCall(javax.servlet.http.HttpServletRequest,
-	 *      aero.deniz.cos.libraries.dto.AbstractDto, String, String)
-	 */
-	protected void logServiceCallGenericObject(HttpServletRequest request,
-			String businessObjectType, String businessObjectTechnicalPk,
-			String info, String event, String systemReaction) {
-		logServiceCallInfo(request.getPathInfo(), businessObjectType + ":"
-				+ businessObjectTechnicalPk, info, event, systemReaction);
-	}
+    /**
+     * Logs a service call that deals with a generic object on INFO level.
+     * <p/>
+     * This method should only be used in exceptional cases where you do not
+     * have a dto object. Usually you would use
+     * ControllerTemplate#logServiceCall.
+     *
+     * @param request                   The HttpServletRequest that will be used to extract the
+     *                                  service url, e.g. "flight.newReportBackOffice/header/next"
+     * @param businessObjectType        e.g. "SearchField"
+     * @param businessObjectTechnicalPk The technical key used ito identify the business object e.g.
+     *                                  "45"
+     * @param info                      Headline type info about the business object, e.g.
+     *                                  "reportNumber"
+     * @param event                     The event that happened, e.g. "User pressed next."
+     * @param systemReaction            How did the system react, e.g.
+     *                                  "Show previous task with errors because report contained validation errors."
+     * @see LoggingTaskControllerTemplate#logServiceCall(javax.servlet.http.HttpServletRequest, com.deniz.framework.dto.AbstractDto, String, String)
+     */
+    protected void logServiceCallGenericObject(HttpServletRequest request,
+                                               String businessObjectType, String businessObjectTechnicalPk,
+                                               String info, String event, String systemReaction) {
+        logServiceCallInfo(request.getPathInfo(), businessObjectType + ":"
+                + businessObjectTechnicalPk, info, event, systemReaction);
+    }
 
-	/**
-	 * Logs a save, updated or delete operation on INFO level. </p>
-	 * 
-	 * @param url
-	 *            The service url, e.g. "flight.newReportBackOffice/header/next"
-	 * @param typeAndPk
-	 *            e.g. 'ReportDto:7887' or 'Field:45'
-	 * @param info
-	 *            should be taken from AbstractDto.getInfo or
-	 *            AbstractEntity.getInfo e.g. "#2011-77845844 'Birdstrike'"
-	 * @param event
-	 *            The event that happened, e.g. "User pressed next."
-	 * @param systemReaction
-	 *            How did the system react, e.g.
-	 *            "Show previous task with errors because report contained validation errors."
-	 */
-	private void logServiceCallInfo(String url, String typeAndPk, String info,
-			String event, String systemReaction) {
-		String logString = "SERVICE: " + url + " BUSINESS-OBJECT (" + typeAndPk
-				+ "): " + info + " EVENT: " + event + " REACTION: "
-				+ systemReaction;
-		logger.info(logString);
-	}
+    /**
+     * Logs a save, updated or delete operation on INFO level. </p>
+     *
+     * @param url            The service url, e.g. "flight.newReportBackOffice/header/next"
+     * @param typeAndPk      e.g. 'ReportDto:7887' or 'Field:45'
+     * @param info           should be taken from AbstractDto.getInfo or
+     *                       AbstractEntity.getInfo e.g. "#2011-77845844 'Birdstrike'"
+     * @param event          The event that happened, e.g. "User pressed next."
+     * @param systemReaction How did the system react, e.g.
+     *                       "Show previous task with errors because report contained validation errors."
+     */
+    private void logServiceCallInfo(String url, String typeAndPk, String info,
+                                    String event, String systemReaction) {
+        String logString = "SERVICE: " + url + " BUSINESS-OBJECT (" + typeAndPk
+                + "): " + info + " EVENT: " + event + " REACTION: "
+                + systemReaction;
+        logger.info(logString);
+    }
 
 }
