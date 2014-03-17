45e0839d0aa3bf685091725c53c24a6bad86a9e3
tryavasu <ugurdeniz.yavas@eu.sony.com>
2014-03-17 16:43:48 +0200

    Configuration

--- essence/Essence-Configuration/src/main/java/com/deniz/framework/configuration/LoggingPropertyPlaceholderConfigurer.java
+++ essence/Essence-Configuration/src/main/java/com/deniz/framework/configuration/LoggingPropertyPlaceholderConfigurer.java
@@ -2,12 +2,9 @@ package com.deniz.framework.configuration;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
@@ -48,29 +45,30 @@ public class LoggingPropertyPlaceholderConfigurer extends PropertyPlaceholderCon
     public void setLocations(Resource[] locations) {
         String stage = System.getProperty("app.configuration.stage");
         if (StringUtils.isEmpty(stage)) {
-            Resource resource = new ClassPathResource("configuration/stages/");
             try {
+                Resource resource = new ClassPathResource("configuration/stages/");
                 File file = resource.getFile();
                 File[] listOfFiles = file.listFiles();
-                String propertyFile = listOfFiles[0].getName();
+                stage = listOfFiles[0].getName();
                 logger.info("app.configuration.stage runtime arguments is null, " +
-                        "so app is using " + propertyFile + " for configuration");
-                propertyFilenames.add(propertyFile);
+                        "so app is using " + stage + " for configuration");
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        } else {
-            Resource stageConfiguration = new ClassPathResource("configuration/stages/app." + stage + ".properties");
-            propertyFilenames.add(stageConfiguration.getFilename());
         }
-        super.setLocations(locations);
+        Resource stageConfiguration = new ClassPathResource("configuration/stages/app." + stage + ".properties");
+        propertyFilenames.add(stageConfiguration.getFilename());
+        locations = (Resource[]) ArrayUtils.add(locations, stageConfiguration);
+
         for (Resource location : locations) {
             propertyFilenames.add(location.getFilename());
         }
+        super.setLocations(locations);
     }
 
     public List<String> getPropertyFilenames() {
         return propertyFilenames;
     }
+
 }
 
